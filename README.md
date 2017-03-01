cc8a6db44f797148ec6f8bedc9be6829d7043196
Leo Davidson <leo@pretentiousname.com>
2017-03-01 15:59:40 +0000

    Initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+### discourse-forcemoderation
+
+This Discourse plugin lets you force all posts by particular, named users to go through moderation.
+
+This is useful if you don't want to make all Trust Level 0 users go through moderation but have
+someone whose posts aren't completely worthless all of the time but need filtering to reduce
+annoyance and disruption.
+
+## License
+
+MIT
---
+++ plugin.rb
@@ -0,0 +1,28 @@
+# name: discourse-forcemoderation
+# about: Force posts from specified usernames to go through moderation.
+# version: 0.1
+# authors: Leo Davidson
+# url: https://github.com/leodavidson/discourse-forcemoderation
+
+enabled_site_setting :force_moderation_enabled
+
+after_initialize do
+
+  module ::DiscourseForceModeration
+    def user_needs_approval?(manager)
+      superResult = super
+      return superResult if ((!(SiteSetting.force_moderation_enabled)) || superResult)
+
+      if SiteSetting.force_moderation_users.is_a? String
+        userName = manager.user.username.downcase
+        userArray = SiteSetting.force_moderation_users.downcase.split("|")
+        return true if (userArray.include? userName)
+      end
+
+      return false
+    end
+  end
+
+  NewPostManager.singleton_class.prepend ::DiscourseForceModeration
+
+end
